c2711dd7dadb37adafbc7c54f610ea06f538daf2
nikhilrajbhar17 <nikhilrajbhar135@gmail.com>
2024-01-25 05:27:44 +0530

    done for the day

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,22 @@
-exports.getProducts = (req, res, next) => {
+const product = require("../models/product");
+const Product = require("../models/product");
+
+// create new product => api/v1/product/new
+
+exports.newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(2001).json({
+    success: true,
+    product,
+  });
+};
+// get all products > api/v1/getproducts
+exports.getProducts = async (req, res, next) => {
+  const products = await product.find();
   res.status(200).json({
     success: true,
-    message: "this is routes will show all products", 
+    count: products.length,
+    products,
   });
 };
---
+++ backend/models/product.js
@@ -0,0 +1,93 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongooose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter product name"],
+    trim: true,
+    maxLength: [100, "Product name cannot exceed 100 characters"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter product price"],
+    maxLength: [5, "Product name cannot exceed 5 characters"],
+    default: 0.0,
+  },
+  Description: {
+    type: Number,
+    required: [true, "Please enter product Description"],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      public_id: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: ["true", "Please select category for this product"],
+    enum: {
+      values: [
+        "Electronics",
+        "Cameras",
+        "Laptops",
+        "Accessories",
+        "Headphones",
+        "Food",
+        "Books",
+        "Cloths/Shoes",
+        "Beauty/health",
+        "Sports",
+        "Outdoor",
+        "Home",
+      ],
+      message: "Please select correct category for product",
+    },
+  },
+  seller: {
+    type: String,
+    required: [true, "Please enter product seller"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please enter product stock"],
+    maxLength: [5, "product name cannot exceed 5 characters"],
+    default: 0,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comments: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  createdAt: {
+    type: Num,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
